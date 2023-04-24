0348fe391553e3ab66f67f3c1f605b5f52aa544a
zjria <zakaria.com.bd@gmail.com>
2023-04-24 22:06:03 +0600

    carusel add

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,18 @@
-/***********
-  Making placeholder star specifically red 
-  plz include: https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
-  ****************/
-function updatePlaceHolderFromById(id_dom) {
-    $(id_dom + ' .palceholder').click(function () {
-        $(this).siblings('input').focus();
-    });
-    $(id_dom + ' .form-control').focus(function () {
-        $(this).siblings('.palceholder').hide();
-    });
-    $(id_dom + ' .form-control').change(function () {
-        $(this).siblings('.palceholder').hide();
-    });
-    $(id_dom + ' .form-control').blur(function () {
-        var $this = $(this);
-        if ($this.val().length == 0) {
-            $(this).siblings('.palceholder').show();
+$(".img-carousel").owlCarousel({
+    margin: 15,
+    loop: true,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    navText: ["<b>⟵</b>", "<b>⟶</b>"],
+    responsive: {
+        0: { items: 1.5 },
+        600: { items: 2.5 },
+        900: {
+            items: 2.5,
+            marge: true,
         }
-    });
-    $(id_dom + ' .form-control').blur();
-}
-updatePlaceHolderFromById("#container");
+    },
+});
